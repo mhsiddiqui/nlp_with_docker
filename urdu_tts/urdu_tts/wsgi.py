a422148cf0ade30ec2a82d431c9555624f18daf9
Muhammad Hassan Siddiqui <muhammad.hassan@fiveriverstech.com>
2018-02-16 10:36:31 +0500

    script commented

--- urdu_tts/urdu_tts/wsgi.py
+++ urdu_tts/urdu_tts/wsgi.py
@@ -15,3 +15,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "urdu_tts.settings")
 
 application = get_wsgi_application()
+
+application = DjangoWhiteNoise(application)
